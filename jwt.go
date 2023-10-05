7f3613298158f3575665698c677079581b742793
Jonas <jonasmooney2@gmail.com>
2023-10-05 07:32:07 -0600

    Updating error handling on jwt.go/createJWT

--- jwt.go
+++ jwt.go
@@ -57,6 +57,7 @@ func CreateJWT(username string) ([]byte, error) {
 	ss, err := token.SignedString(keyByte)
 	if err != nil {
 		log.Printf("Error signing token: %v", err)
+		return nil, err
 	}
 
 	responseData := TokenResponse{
