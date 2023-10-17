349ab46953bdb84cc453a07b0383722643ed619d
Jonas <jonasmooney2@gmail.com>
2023-10-17 07:30:09 -0600

    Added email.go config function and comments for testing api key

--- email.go
+++ email.go
@@ -10,23 +10,48 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func SendSignupEmail(username, email string) {
+type Configuration struct {
+	SendGridFromUsername string
+	SendGridFromEmail    string
+	SendGridAPIKey       string
+}
+
+func loadConfig() (Configuration, error) {
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("Error loading .env file")
 	}
 
-	fromUsername := os.Getenv("SENDGRID_FROM_USERNAME")
-	fromEmail := os.Getenv("SENDGRID_FROM_EMAIL")
-	apiKey := os.Getenv("SENDGRID_API_KEY")
+	cfg := Configuration{
+		SendGridFromUsername: os.Getenv("SENDGRID_FROM_USERNAME"),
+		SendGridFromEmail:    os.Getenv("SENDGRID_FROM_EMAIL"),
+		SendGridAPIKey:       os.Getenv("SENDGRID_API_KEY"),
+	}
 
-	from := mail.NewEmail(fromUsername, fromEmail)
+	if cfg.SendGridFromUsername == "" || cfg.SendGridFromEmail == "" || cfg.SendGridAPIKey == "" {
+		return Configuration{}, err
+	}
+
+	return cfg, nil
+}
+
+func SendSignupEmail(username, email string) {
+	cfg, err := loadConfig()
+	if err != nil {
+		log.Println("Error loading config: ", err)
+	}
+
+	// fromUsername := os.Getenv("SENDGRID_FROM_USERNAME")
+	// fromEmail := os.Getenv("SENDGRID_FROM_EMAIL")
+	// apiKey := os.Getenv("SENDGRID_API_KEY")
+
+	from := mail.NewEmail(cfg.SendGridFromUsername, cfg.SendGridFromEmail)
 	subject := "Echo-One-Signup"
 	to := mail.NewEmail(username, email)
 	plainTextContent := "and easy to do anywhere, even with Go"
 	htmlContent := "<h1>Congrats " + username + "! " + "Welcome to Echo One!</h1>"
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-	client := sendgrid.NewSendClient(apiKey)
+	client := sendgrid.NewSendClient(cfg.SendGridAPIKey)
 	response, err := client.Send(message)
 	if err != nil {
 		log.Println("Sendgrid signup email error: ", err)
@@ -34,3 +59,17 @@ func SendSignupEmail(username, email string) {
 		log.Println("Sendgrid signup email status: ", response.StatusCode)
 	}
 }
+
+/*
+Testing Environment:
+Testing Sendgrip api key
+*/
+
+/*
+Production Environment:
+In production, you should have monitoring and alerting systems in place to detect and respond to issues with your email sending functionality. This can include:
+Health Checks: Implement a health check endpoint in your application that periodically verifies the SendGrid API's operational status. This endpoint can be monitored by external systems or load balancers, which can take actions based on the results (e.g., switch to a backup email provider if SendGrid is down).
+Alerting: Set up alerts to notify you when SendGrid experiences issues, such as rate limits exceeded, outages, or significant delivery failures. Services like SendGrid often provide APIs or webhooks to facilitate this kind of monitoring.
+Logging and Error Handling: Your application should have comprehensive error handling and logging for email sending. When errors occur, they should be logged and possibly reported so that you can investigate and address the issue promptly.
+Fallback Mechanism: Consider having a fallback mechanism in place. If SendGrid experiences issues, you can switch to an alternative email provider or service to ensure that user signups are not interrupted.
+*/
